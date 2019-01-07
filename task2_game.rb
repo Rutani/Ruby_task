50c924f8010ad7ed1965cbcc01912cea8e8b6e98
Rutani <rutani.thakkar@bacancytechnology.com>
2019-01-07 16:31:47 +0530

    initial

---
+++ task2_game.rb
@@ -0,0 +1,25 @@
+class Game
+	def my_game 
+		r = Random.new
+		@computer_guess_number=r.rand(5..150)
+		puts @computer_guess_number
+
+		puts "Let's Strat the Game...!!!\n please Guess the number...!!"
+		10.times do |item|		
+			@player_guess_number=gets.to_i
+			if @player_guess_number>@computer_guess_number
+				puts"This is greater then selected number...Try Again"
+			elsif @player_guess_number<@computer_guess_number
+				puts "This is lesser then selected number...Try Again"
+			else
+				puts "Congratulations, You got the number #{@computer_guess_number} in #{item+1} attempts(try)"	
+				break;
+			end 
+			if item==9
+			puts "sorry!!! You lose this game...!!"
+			end
+		end
+	end
+end
+game=Game.new
+game.my_game
