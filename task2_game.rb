27add0b27f15e399679ec3248ecd04e3fbc6a99d
Rutani <rutani.thakkar@bacancytechnology.com>
2019-01-07 17:55:05 +0530

    some changes

--- task2_game.rb
+++ task2_game.rb
@@ -7,10 +7,20 @@ class Game
 		puts "Let's Strat the Game...!!!\n please Guess the number...!!"
 		10.times do |item|		
 			@player_guess_number=gets.to_i
-			if @player_guess_number>@computer_guess_number
+			if @player_guess_number>@computer_guess_number 
+				if item==9
+
+				else
 				puts"This is greater then selected number...Try Again"
+				end
 			elsif @player_guess_number<@computer_guess_number
+
+				if item==9
+				else
 				puts "This is lesser then selected number...Try Again"
+				end
+
+				
 			else
 				puts "Congratulations, You got the number #{@computer_guess_number} in #{item+1} attempts(try)"	
 				break;
